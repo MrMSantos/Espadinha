8d6923d1441dec8cde50edcb5588cee13b830015
MrMSantos <manuel.jss@hotmail.com>
2017-05-04 16:10:21 +0100

    deck updated

--- deck.py
+++ deck.py
@@ -2,11 +2,16 @@
 
 import random
 
+S = '♠'
+C = '♣'
+H = '♥'
+D = '♦'
+
 values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 
 def create():
-    return ['AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC','JC', 'QC', 'KC',\
-            'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH', 'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD','JD', 'QD', 'KD']
+    return ['A'+S, '2'+S, '3'+S, '4'+S, '5'+S, '6'+S, '7'+S, '8'+S, '9'+S, 'T'+S, 'J'+S, 'Q'+S, 'K'+S, 'A'+C, '2'+C, '3'+C, '4'+C, '5'+C, '6'+C, '7'+C, '8'+C, '9'+C, 'T'+C,'J'+C, 'Q'+C, 'K'+C,\
+            'A'+H, '2'+H, '3'+H, '4'+H, '5'+H, '6'+H, '7'+H, '8'+H, '9'+H, 'T'+H, 'J'+H, 'Q'+H, 'K'+H, 'A'+D, '2'+D, '3'+D, '4'+D, '5'+D, '6'+D, '7'+D, '8'+D, '9'+D, 'T'+D,'J'+D, 'Q'+D, 'K'+D]
 
 def shuffle(deck):
     random.shuffle(deck)
@@ -19,3 +24,6 @@ def remove_card(deck, card):
 
 def get_card(deck, pos):
     return deck[pos]
+
+d = create()
+print(d)
